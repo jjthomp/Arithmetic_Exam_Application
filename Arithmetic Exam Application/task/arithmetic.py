61453c736f4e1b44aee4fe6f931b4face083e07d
jjthomp <jjthompson124@gmail.com>
2022-12-31 13:59:51 -0800

    added playing multiple levels

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,21 +1,41 @@
 import random
 operations = ["+", "-", "*"]
-n = 0
-format = False
+n1 = 0
+n2 = 0
+correct_format = False
+correct_format2 = False
 response = 0
 count = 0
 pick = False
 level = 0
+levels_attempt = []
 leveldict = {
   1: "simple operations with numbers 2-9",
-  2: "integral squares of 11-29"
+  2: "integral squares of 1-12"
 }
+
+
+def question_asker(question):
+    print(question)
+    response3 = False
+    while not response3:
+        save = input()
+        if save == "yes" or save == "Yes" or save == "y" or save == "YES":
+            response3 = True
+            return True
+        if save == "no" or save == "n" or save == "No" or save == "NO":
+            response3 = True
+            return False
+
+        else:
+            print("Incorrect format")
+    return False
 
 
 while not pick:
     print("""Which level do you want? Enter a number:
     1 - simple operations with numbers 2-9
-    2 - integral squares of 11-29""")
+    2 - integral squares of 1-12""")
     try:
         level = int(input())
         pick = True
@@ -34,13 +54,13 @@ if level == 1:
         op = random.choice(operations)
         print(f"{a} {op} {b}")
 
-        while not format:
+        while not correct_format:
             try:
                 response = int(input())
-                format = True
+                correct_format = True
             except ValueError:
                 print("Wrong format! Try again")
-                format = False
+                correct_format = False
 
         if op == "+":
             result = int(a) + int(b)
@@ -48,40 +68,72 @@ if level == 1:
             result = int(a) - int(b)
         if op == "*":
             result = int(a) * int(b)
+
         if response == result:
             print("Right!")
-            n += 1
+            n1 += 1
         else:
             print("Wrong!")
         count += 1
-        format = False
+        correct_format = False
+
+    levels_attempt.append(1)
+    response2 = question_asker("Would you like to try level 2? Enter yes or no:")
+    if response2:
+        level = 2
+        count = 0
+
 
 if level == 2:
     while count < 5:
-        a = random.randint(11, 29)
+        a = random.randint(1, 12)
         print(a)
-        while not format:
+        while not correct_format2:
             try:
                 response = int(input())
-                format = True
+                correct_format2 = True
             except ValueError:
                 print("Wrong format! Try again.")
-                format = False
+                correct_format2 = False
 
         result = a*a
         if response == result:
             print("Right!")
-            n += 1
+            n2 += 1
         else:
             print("Wrong!")
         count += 1
-        format = False
+        correct_format2 = False
 
-print(f"Your mark is {n}/5. Would you like to save the result? Enter yes or no.")
-save = input()
-if save == "yes" or save == "Yes" or save == "y" or save == "yes":
+    levels_attempt.append(2)
+
+
+if len(levels_attempt) == 2:
+    print(f"Your mark is {n1 + n2}/10.")
+elif levels_attempt.__contains__(1):
+    print(f"Your mark is {n1}/5.")
+else:
+    print(f"Your mark is {n2}/5.")
+
+response1 = question_asker("Would you like to save your result to the file? Enter yes or no.")
+
+if response1:
     print("What is your name?")
     name = input()
     with open('results.txt', 'a') as f:
-        f.write(f'{name}: {n}/5 in level {level} ({leveldict[level]}). ')
+        if levels_attempt.__contains__(1):
+         f.write(f'{name}: {n1}/5 in level 1 ({leveldict[1]}). ')
+        if levels_attempt.__contains__(2):
+            f.write(f'{name}: {n2}/5 in level 2 ({leveldict[2]}). ')
+        if len(levels_attempt) == 2:
+            f.write(f"{name}'s total mark was {n1 + n2}/10.")
+        elif levels_attempt.__contains__(1):
+            f.write(f"{name}'s total mark was {n1}/5.")
+        else:
+            f.write(f"{name}'s total mark was {n2}/5.")
+
     print('The results are saved in "results.txt".')
+    print("Exiting Program")
+    if not response1:
+        print("Exiting program")
+
